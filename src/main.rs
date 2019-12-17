b0fee65f988782e0d8e04b79d46a01b36f643488
Thomas BRAZE <thomas.braze@opalesystems.com>
2019-12-17 15:50:01 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+extern crate clap;
+use std::process::{Command, Stdio};
+use clap::{App, Arg, ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = arguments();
+    let input = matches.value_of("input").unwrap();
+    let output = matches.value_of("output").unwrap();
+
+    let mut in_args: Vec<&str> = input.split(" ").collect();
+    let mut out_args: Vec<&str> = output.split(" ").collect();
+
+    let in_command = in_args.remove(0);
+    let out_command = out_args.remove(0);
+
+    let in_command = Command::new(&in_command[..])
+        .args(&in_args)
+        .output()
+        .expect("Error in the input command");
+
+    let input = String::from_utf8_lossy(&in_command.stdout);
+
+    let out_command = Command::new( out_command )
+        .arg(&input[..])
+        .args(out_args)
+        .stdin(Stdio::piped())
+        .output()
+        .expect("Error in the output command");
+
+    println!("{}", String::from_utf8_lossy(&out_command.stdout));
+}
+
+fn arguments() -> ArgMatches<'static> {
+
+    let arguments = App::new("mypipe")
+        .version("1.0.0")
+        .about("A Unix like command pipe in RUST")
+        .author("Thomas BRAZE <thomas.braze@myges.fr>")
+        .arg( Arg::with_name("input")
+                  .short("in")
+                  .long("input")
+                  .value_name("input")
+                  .help("input command")
+                  .takes_value(true)
+                  .required(true)
+        ).arg( Arg::with_name("output")
+        .short("out")
+        .long("output")
+        .value_name("output")
+        .help("output command")
+        .takes_value(true)
+        .required(true)
+    )
+        .get_matches();
+
+    return  arguments;
 }
